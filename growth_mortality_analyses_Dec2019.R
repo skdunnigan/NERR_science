63f7f0b88d0274662c2d3ece23c340f2b933d2fa
Will White <jwilsonwhite@gmail.com>
2019-12-12 10:27:15 -0500

    Update analysis code
    Update analysis code to include export of data files needed for EPR model

--- growth_mortality_analyses_Dec2019.R
+++ growth_mortality_analyses_Dec2019.R
@@ -127,83 +127,13 @@ quartz(width=6,height=6)
 ggarrange(Gp2[[7]],Gp2[[2]],Gp2[[6]],Gp2[[5]],Gp2[[1]],Gp2[[3]],Gp2[[4]], ncol = 3)
 
 #---------------------------------
-# Revisit this to split mortality into early vs. late
-# 1.1) Do 'post-juvenile' mortality using later dates:
-# Load in data.
-data = read_xlsx("2019_outplant(updated3dec).xlsx",sheet="oysters")
-
-# clean up data formatting
-data$reef.id = as.factor(data$reef.id) # which zone within GTM
-data$treatment = as.factor(data$treatment) # cage, control, cagecontrol
-data$meter = as.factor(data$meter) # within-site replication (at which transect meter was the experimental unit)
-
-# define some organizational variables
-zones = levels(data$reef.id)
-
-# strip weird NA rows & bad values (zeros)
-NArows = data$reef.id==""
-data = data[!NArows,]
-Zrows  =data$size == 0
-data = data[!Zrows,]
-
-# Compile data for analysis by GLM. Take count of number of remaining oysters 
-# on each data, at each replicate experimental unit
-data$alive <- data$status==1
-data.old <- subset(data,timedays>=75)
-# Rescale time so that it runs from zero onwards at each site
-for (i in 1:length(zones)){
- min.temp <- subset(data.old,reef.id=zones[i])
- min.date <- min(min.temp$timedays)
- data.old$timedays = data.old$timedays-min.date
-}
-data.agg <- aggregate(data.old$alive,by=list(data.old$reef.id,data.old$treatment,data.old$timedays,data.old$meter),FUN=sum)
-colnames(data.agg) <- c('reef.id','treatment','timedays','meter','alive')
-
-# Do GLMs:
-# pre-allocate variables for storing results
-sitelist <- vector()
-optionlist <- vector()
-M <- vector()
-Ms <- matrix(nrow=length(zones),ncol=2)
-intercept <- vector()
-Mod_mor <- list()
-Mor_cov <- vector()
-k <- 1 # this is a counter variable for use in a loop
-
-treatments <- c('cage','control','cagecontrol') # for this analysis we do not consider the cagecon treatment
-treatnames <- c('cage','con','cagecon') # short names
-
-# Loop over Zones & Treatments to estimate survival curve
-for (i in 1:length(zones)){ 
-  for (j in 1:2){ #length(treatments)){ 
-    
-    data.sub <- subset(data.agg,reef.id == zones[i] & treatment == treatments[j])
-    m = glm(alive ~ timedays, family=poisson(link='log'),data = data.sub)
-    Mod_mor[[k]] <- m
-    Mor_cov[k] <- vcov(m)[2,2]
-    #mod_mortality[[i,j]] <- m
-    sitelist[k] <- zones[i]
-    optionlist[k] <- treatnames[j]
-    M[k] <- coef(m)[2] # for export to csv
-    Ms[i,j] <- coef(m)[2] # for use in plotting later
-    intercept[k] <- coef(m)[1]
-    k = k+1
-    
-  }
-}
-
-NERRmortalitycoeffs_adult <- data.frame(sitelist,optionlist,M,intercept,Mor_cov)
-write.csv(NERRmortalitycoeffs_adult,'NERRmortalitycoeffs2019dec_adult.csv')
-
-# As of Dec 2019, this estimation is problematic -- too little variation in the data, too few samples after 75 d.
-# For results with plausible values, adult mortality appears to be ~10% of juvenile mortalilty.
-
+# To be added: separate adult mortality analysis
 #---------------------------------
 
 
 
 #---------------------------------------------------------
-# 1) Growth analysis. From 2018 outplant
+# 3) Growth analysis. From 2018 outplant
 # Load in data.
 data = read_xlsx("2018_outplant(updated_3dec).xlsx",sheet="cluster")
 
@@ -256,9 +186,6 @@ for (z in 1:length(zones)){
   # Results for plotting:
   L.dummy[,z] = Linf[z]*(1-exp(-(k[z]*(age.dummy-t0[z]))))  }
 
-#temporarily commented out by Laura
-#L.dummy[110:405,5] = NA # blank out some of the Salt R harvest data past the point of all oysters dying
-
 # Plot data &  growth curves:
 Gp <- list()
 Colors <- c('green','magenta','blue','purple','black','yellow','red','black','cyan')
@@ -288,177 +215,23 @@ ggarrange(Gp[[9]],Gp[[8]],Gp[[2]],Gp[[7]],Gp[[6]],Gp[[5]],Gp[[1]],Gp[[3]],Gp[[4]
 Order = c(7,2,6,5,1,3,4)
 quartz(width=6,height=6)
 ggarrange(Gp[[7]],Gp[[2]],Gp[[6]],Gp[[5]],Gp[[1]],Gp[[3]],Gp[[4]], ncol = 3)
-
-
-# Fit vB and get fitted line
-Linf <- rep(NA,length(Zones)) # allocate vectors for results
-k <- Linf; t0 <- Linf
-# Put in some fake guesses for Pellicer?
-
-#Linf.starts = rep(80,length(Zones)); Linf.starts[4]=10 # try different starting values for different sites
-age.dummy = 0:105
-L.dummy = matrix(0,nrow=length(age.dummy),ncol=length(Zones))
-Mod_growth <- list()
-for (z in 1:length(Zones)){ 
-  #if (z != 4){# pellicer zone doesn't have enough data to fit
-  data.sub = subset(data,reefid==Zones[z] & treatment == Treat[1]) #just trying to do cage for now since 2018 data file doesnt have treatment
-  
-#replace nls with nlme so you can have random effect of meter   
-f1 <- data.sub$size ~ Linf*(1-exp(-(k*(data.sub$timedays - t0))))
-m = nlme(f1,data=data.sub,start=list(k=0.01,Linf=100,t0=0),fixed=k+Linf+t0~1,random=meter~1,na.action=na.omit)#,group=~reefid,na.action=na.omit)
-#m = nls(formula = data.sub$size ~ Linf*(1 - exp(-k*(data.sub$timedays -t0))), 
-        #start = list(Linf=50,k=0.01,t0=0),data=data.sub,control=nls.control(minFactor=1/(2^15),maxiter = 500))
-Mod_growth[[z]] <- m
-Linf[z] = coef(m)[1]
-k[z] = coef(m)[2]
-t0[z] = coef(m)[3]#}
-L.dummy[,z] = Linf[z]*(1-exp(-(k[z]*(age.dummy-t0[z]))))  }
-
-#L.dummy[110:405,5] = NA # blank out some of the Salt R harvest data past the point of all oysters dying
-
-# Plot vs. time to estimate growth, including growth curves:
-Gp <- list()
-Colors <- c('green','magenta','blue','purple','black','yellow','red','black','cyan')
-Zone_names = c('Butler','Guana R.','Matanzas R.','Pellicer','Salt R.','St. Augustine','Tolomato R.')
-
-
-for (z in 1:length(Zones)){ 
-  data.sub = subset(data,reefid==Zones[z] & treatment == Treat[1])
-  data.dummy = data.frame(x = age.dummy,y=L.dummy[,z])
-Gp[[z]]<-ggplot(data.sub,aes(x=timedays,y=size))+
-  geom_jitter(color=Colors[z])+
-  geom_line(data=data.dummy,aes(x=x,y=y),color='blue')+
-  ylim(c(0,70))+
-  xlab('Days')+
-  ylab('Length (mm)')+
-  ggtitle(Zone_names[z])
-  theme_bw()}
-
-# How to order sites:
-# 9 (tolo.non), 8 (tolo.harvest),2 (guana), 7 (staug), 6 (salt.non), 5 (salt.harvest), 1 (butler), 3 (matanzas), 4 (pellicer)
-Order = c(7,2,6,5,1,3,4)
-quartz(width=5,height=3)
-ggarrange(Gp[[7]],Gp[[2]],Gp[[6]],Gp[[5]],Gp[[1]],Gp[[3]],Gp[[4]], ncol = 2)
+#---------------------------------------------------------------------------
 
 
 
+#---------------------------------------------------------------------------
+# 3) Create output Params & CovMats files for the model
+Params <- data.frame(sites=zones, M, Linf, k) # maximum likelihood estimates for each parameter
 
+# CovMats will have to be a list of matrices
+CovMats = list()
+for (i in 1:length(zones)){
+  Ctmp <- matrix(0,nrow=3,ncol=3)
+  Ctmp[1,1] = Mor_cov[i] # variance in mortality rate
+  Ctmp[2:3,2:3] = Growth_cov[[i]]
+  CovMats[[i]] = Ctmp
+}
 
-# # SURVIVAL ####
-# data = read.csv("outplant.data_updated2sept.oysters.csv")
-# data$elapsed[data$elapsed<0]=0
-# data$meter = factor(data$meter)
-# 
-# # remove anything with "2" in it from status
-# data1 = data%>%
-#   filter(status !=2) %>%
-#   group_by(reef.id, elapsed, treatment, meter)%>%
-#   summarize(
-#     avg.surv = mean(status, na.rm = TRUE)
-#   )
-# 
-# 
-# # Plot vs. time to estimate growth:
-# ggplot(data1,aes(x=elapsed,y=avg.surv))+
-#   geom_jitter(aes(color=reef.id))+
-#   theme_bw()
-# 
-# ZonesM = levels(data1$reef.id)
-# # Fit exp(-M) and get fitted line
-# M <- rep(0,length(ZonesM)) # allocate vector for results
-# age.dummy = 0:105
-# Surv.dummy = matrix(0,nrow=length(age.dummy),ncol=length(ZonesM))
-# Mod_survival <- list()
-# for (z in 1:length(ZonesM)){ 
-#     data.sub = subset(data1,reef.id==ZonesM[z]&treatment=='control')
-#     m = nls(formula = data.sub$avg.surv ~ exp(-M*elapsed), 
-#             start = list(M=0.2),data=data.sub)
-#     M[z] = coef(m)[1]
-#     Mod_survival[[z]] <- m
-#   Surv.dummy[,z] = exp(-M[z]*age.dummy)  }
-# 
-# # Fit exp(-M) and get fitted line (CAGE data)
-# Mc <- rep(0,length(ZonesM)) # allocate vector for results
-# Modc_survival <- list()
-# age.dummy = 0:105
-# Surv.dummy.c = matrix(0,nrow=length(age.dummy),ncol=length(ZonesM))
-# for (z in 1:length(ZonesM)){ 
-#   data.sub = subset(data1,reef.id==ZonesM[z]&treatment=='cage')
-#   m = nls(formula = data.sub$avg.surv ~ exp(-M*elapsed), 
-#           start = list(M=0.2),data=data.sub)
-#   Modc_survival[[z]] <- m
-#   Mc[z] = coef(m)[1]
-#   Surv.dummy.c[,z] = exp(-Mc[z]*age.dummy)  }
-# 
-# # Plot vs. time to estimate growth, including growth curves (CONTROL ONLY):
-# Gpm <- list()
-# Colors <- c('green','magenta','blue','purple','yellow','red','cyan')
-# Zone_names = c('Butler','Guana R.','Matanzas R.','Pellicer','Salt R.','St. Augustine','Tolomato R.')
-# for (z in 1:length(ZonesM)){ 
-#   data.sub = subset(data1,reef.id==ZonesM[z]&treatment=='control')
-#   data.dummy = data.frame(x = age.dummy,y=Surv.dummy[,z])
-#   Gpm[[z]]<-ggplot(data.sub,aes(x=elapsed,y=avg.surv))+
-#     geom_jitter(color=Colors[z])+
-#     geom_line(data=data.dummy,aes(x=x,y=y),color='blue')+
-#     ylim(c(0,1))+
-#     xlab('Days')+
-#     ylab('Average survival')+
-#     ggtitle(Zone_names[z])
-#     theme_bw()}
-# 
-# # How to order sites:
-# # 7 (tolo), 2 (guana), 6 (staug), 5 (salt), 1 (butler), 3 (matanzas), 4 (pellicer)
-# Order = c(7,2,6,5,1,3,4)
-# quartz(width=5,height=3)
-# ggarrange(Gpm[[7]],Gpm[[2]],Gpm[[6]],Gpm[[5]],Gpm[[1]],Gpm[[3]],Gpm[[4]], ncol = 3)
-# 
-# # Plot vs. time to estimate growth, including growth curves (CAGE + CONTROL):
-# Gpmc <- list()
-# Colors <- c('green','magenta','blue','purple','yellow','red','cyan')
-# Zone_names = c('Butler','Guana R.','Matanzas R.','Pellicer','Salt R.','St. Augustine','Tolomato R.')
-# for (z in 1:length(ZonesM)){ 
-#   data.sub1 = subset(data1,reef.id==ZonesM[z]&treatment=='control')
-#   data.sub2 = subset(data1,reef.id==ZonesM[z]&treatment=='cage')
-#   data.dummy = data.frame(x = age.dummy,y=Surv.dummy[,z])
-#   data.dummyc = data.frame(x = age.dummy,y=Surv.dummy.c[,z])
-#   Gpmc[[z]]<-ggplot(data.sub1,aes(x=elapsed,y=avg.surv))+
-#     geom_jitter(color=Colors[z])+
-#     geom_line(data=data.dummy,aes(x=x,y=y),color='blue')+
-#     geom_jitter(data=data.sub2,aes(x=elapsed,y=avg.surv),color=Colors[z],shape=21)+
-#     geom_line(data=data.dummyc,aes(x=x,y=y),color='blue',linetype=2)+
-#     ylim(c(0,1))+
-#     xlab('Days')+
-#     ylab('Average survival')+
-#     ggtitle(Zone_names[z])
-#   theme_bw()}
-# 
-# # How to order sites:
-# # 7 (tolo), 2 (guana), 6 (staug), 5 (salt), 1 (butler), 3 (matanzas), 4 (pellicer)
-# Order = c(7,2,6,5,1,3,4)
-# quartz(width=5,height=3)
-# ggarrange(Gpmc[[7]],Gpmc[[2]],Gpmc[[6]],Gpmc[[5]],Gpmc[[1]],Gpmc[[3]],Gpmc[[4]], ncol = 3)
-# 
-# #Save all the coefficients and covariance matrices to transport over to Matlab
-# Growth_Cell <- list()
-# Coeff_Growth_Cell <- list()
-# for (z in 1:length(Mod_growth)){
-#     {if(z!=4) #the 4th is that site that starts with a p and there isn't a model fit 
-#   Growth_Cell[[z]] <- vcov(Mod_growth[[z]])
-#   Coeff_Growth_Cell[[z]] <- coef(Mod_growth[[z]])
-#     }
-# }
-# 
-# Survival_Cell <- list()
-# CSurvival_Cell <- list()
-# Coeff_Survival_Cell <- list()
-# Coeff_CSurvival_Cell <- list()
-# for (z in 1:length(Mod_survival)){
-#     Survival_Cell[[z]] <- vcov(Mod_survival[[z]])
-#     Coeff_Survival_Cell[[z]] <- coef(Mod_survival[[z]])
-#     CSurvival_Cell[[z]] <- vcov(Modc_survival[[z]])
-#     Coeff_CSurvival_Cell[[z]] <- coef(Modc_survival[[z]])
-# }
-
-  
-  
-
+save(Params,'Params.Rdata')
+save(CovMats,'CovMats.Rdata')
+#---------------------------------------------------------------------------
